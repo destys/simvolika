09c9b72041607d9d2ce58d59def0b7bb26f6aa76
Данил Хальпуков <danil@MacBook-Air-Danil.local>
2023-06-09 14:10:49 +0300

    In progress

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,16 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+/* 
+const mpList = document.querySelector('.mp');
+ */
+const mpBtns = document.querySelectorAll('[data-mp]');
+
+if (mpBtns.length) {
+    mpBtns.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            btn.classList.toggle('_active');
+        });
+    })
+}
+
